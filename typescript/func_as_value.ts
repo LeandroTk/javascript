cddf1cc5c28acfe7a1ef41c92423b64e56f03af4
TK <leandrotk100@gmail.com>
2019-12-04 22:55:47 -0300

    fix

--- typescript/func_as_value.ts
+++ typescript/func_as_value.ts
@@ -26,7 +26,7 @@ class B {
 
 class A {
   a1: number;
-  b1: B;
+  b1: B | objectB;
 
   private static doSomething({ b, c }: objectB, func: ({ b, c }: objectB) => B | objectB = B.build) {
     return func({ b, c });
@@ -46,10 +46,10 @@ class A {
 }
 
 const instanceA = new A(1, 2, 3);
-console.log(instanceA instanceof A);
-console.log(instanceA.a1)
-console.log(instanceA.b1)
+console.log(instanceA instanceof A); // true
+console.log(instanceA.a1) // 1
+console.log(instanceA.b1) // B { b: 2, c: 3 }
 
 const objectA = A.toJS(0, -1, 99);
-console.log(objectA instanceof A);
-console.log(objectA)
+console.log(objectA instanceof A); // false
+console.log(objectA) // { a1: 0, b1: { b: -1, c: 99 } }
