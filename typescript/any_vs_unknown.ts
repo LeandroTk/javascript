d11b0f555b89fa510cf793e5e952caa1edfb6895
TK <leandrotk100@gmail.com>
2020-02-01 22:46:10 -0300

    unknown

--- typescript/any_vs_unknown.ts
+++ typescript/any_vs_unknown.ts
@@ -1,12 +1,31 @@
-let value: any;
+// Reference: https://mariusschulz.com/blog/the-unknown-type-in-typescript
 
-value = true;            // OK
-value = 42;              // OK
-value = "Hello World";   // OK
-value = [];              // OK
-value = {};              // OK
-value = Math.random;     // OK
-value = null;            // OK
-value = undefined;       // OK
-value = new TypeError(); // OK
-value = Symbol("type");  // OK
+// Any
+let anyValue: any;
+
+anyValue = true;             // OK
+anyValue = 42;               // OK
+anyValue = "Hello World";    // OK
+anyValue = [];               // OK
+anyValue = {};               // OK
+anyValue = Math.random;      // OK
+anyValue = null;             // OK
+anyValue = undefined;        // OK
+
+anyValue.foo.bar;  // OK
+anyValue.trim();   // OK
+anyValue();        // OK
+new anyValue();    // OK
+anyValue[0][1];    // OK
+
+// Unknown
+let unknownValue: unknown;
+
+let unknownValue1: unknown = unknownValue;   // OK
+let unknownValue2: any = unknownValue;       // OK
+let unknownValue3: boolean = unknownValue;   // Error
+let unknownValue4: number = unknownValue;    // Error
+let unknownValue5: string = unknownValue;    // Error
+let unknownValue6: object = unknownValue;    // Error
+let unknownValue7: any[] = unknownValue;     // Error
+let unknownValue8: Function = unknownValue;  // Error
