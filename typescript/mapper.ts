fdc5814b35e46049e95f0b20187ba02f14c47d57
TK <leandrotk100@gmail.com>
2020-03-29 23:24:33 -0300

    mapper

--- typescript/mapper.ts
+++ typescript/mapper.ts
@@ -1,10 +1,10 @@
-// mapper to map from API contract to APP contract
-
 type PersonAPI = {
   id: number,
-  nome: string,
+  name: string,
+  lastname?: string,
   email: string,
-  ativo: boolean
+  status: string,
+  lastVisit: Date
 };
 
 type Person = {
@@ -14,19 +14,44 @@ type Person = {
   active: boolean
 };
 
-const fromAPI = (person: PersonAPI): Person => ({
-  id: person.id,
-  name: person.nome,
-  email: person.email,
-  active: person.ativo
-});
+enum PersonStatus {
+  Active = 'active',
+  Inactive = 'inactive'
+};
+
+const buildPersonName = (person: PersonAPI): string =>
+  person.lastname
+    ? `${person.name} ${person.lastname}`
+    : person.name;
+
+const isActive = (person: PersonAPI): boolean => {
+  const oneDayMilliseconds: number = 24 * 3600 * 1000;
+  const sevenDaysMilliseconds: number = oneDayMilliseconds * 7;
+  const weekAgo: Date = new Date(Date.now() - sevenDaysMilliseconds);
+
+  return person.status === PersonStatus.Active &&
+    person.lastVisit >= weekAgo;
+};
+
+const fromAPI = (person: PersonAPI): Person => {
+  const name: string = buildPersonName(person);
+  const active: boolean = isActive(person);
+
+  return {
+    id: person.id,
+    name,
+    email: person.email,
+    active
+  };
+};
 
 const payloadAPI = {
   id: 1,
-  nome: 'TK',
+  name: 'TK',
+  lastname: 'Kinoshita',
   email: 'tk@mail.com',
-  ativo: true
+  status: 'active',
+  lastVisit: new Date()
 };
 
 const person = fromAPI(payloadAPI);
-console.log(person);
